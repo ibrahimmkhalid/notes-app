bb35450e32b5f488507264714a7310e24ee859e9
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2022-06-29 10:45:14 +0500

    updated search api

--- server/routes/search.js
+++ server/routes/search.js
@@ -5,15 +5,12 @@ let Note = require('../models/note.models.js');
 
 router.route('/').get((req, res) => {
   const query = req.query.q;
-  Note.aggregate().
-    search({
-      text: {
-        query: query,
-        path: 'notes'
-      }
-    })
-    .then(notes => res.json(notes))
-    .catch(err => res.status(400).json('Error: ' + err));
+  Note.find().or([
+    {title: {$regex: '.*' + query + '.*', $options: 'i'}},
+    {text: {$regex: '.*' + query + '.*', $options: 'i'}}
+  ])
+  .then(notes => res.json(notes))
+  .catch(err => res.status(400).json('Error: ' + err));
 
 });
 
