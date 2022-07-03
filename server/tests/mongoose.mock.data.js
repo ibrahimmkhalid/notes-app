e485519c6d79f58dd1c9c3581ea285bad554df69
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2022-07-04 00:01:47 +0500

    i dont really know what im doing

---
+++ server/tests/mongoose.mock.data.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+const Note = require("../models/note.models.js")
+const User = require("../models/user.models.js")
+
+function async createMockUser(data) {
+  return await 
+}
--- server/tests/notes.test.js
+++ server/tests/notes.test.js
@@ -1,64 +1,37 @@
 const Note = require("../models/note.models.js");
 const NoteService = require("../service/notes.js");
+const mockingoose = require('mockingoose');
+const mongoose = require('mongoose');
 
 describe('Get all notes', () => {
-  Note.find = jest.fn().mockReturnValue([
+  let _user1_id = new mongoose.Types.ObjectId();
+  let _user2_id = new mongoose.Types.ObjectId();
+
+  let _data = [
     {
       title: 'Note 1',
       text: 'text for note 1',
-      owner: 1
+      owner: _user1_id
     },{
       title: 'Note 2',
       text: 'text for note 2',
-      owner: 2
+      owner: _user2_id
     },{
       title: 'Note 3',
       text: 'text for note 3',
       owner: null
     }
-  ]);
+  ];
+
 
   it.only('Should show list of all notes if user is admin', async () => {
+    mockingoose(Note).toReturn(_data, 'find');
     user = {};
-    user.id = 1;
+    user.id = _user1_id;
     user.username = "user"
     user.admin = true;
 
     const data = await NoteService.getAllNotes(user);
-    await expect(data).toStrictEqual([
-      {
-        title: 'Note 1',
-        text: 'text for note 1',
-        owner: 1
-      },{
-        title: 'Note 2',
-        text: 'text for note 2',
-        owner: 2
-      },{
-        title: 'Note 3',
-        text: 'text for note 3',
-        owner: null
-      }
-    ]);
+    await expect(data).toMatchObject(_data);
   });
-
-  it.only('Should show list of all notes available to a user', async () => {
-    user = {};
-    user.id = 1;
-    user.username = "user"
-    user.admin = false;
-
-    const data = await NoteService.getAllNotes(user);
-    await expect(data).toStrictEqual([
-      {
-        title: 'Note 1',
-        text: 'text for note 1',
-        owner: 1
-      },{
-        title: 'Note 3',
-        text: 'text for note 3',
-        owner: null
-      }
-    ]);
-  });
-})
+});
