4392661fb4c070094a34256a1cfbd8b796138080
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2022-07-04 18:39:55 +0500

    oops, this wasnt supposed to be committed

--- server/tests/mongoose.mock.data.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-const Note = require("../models/note.models.js")
-const User = require("../models/user.models.js")
-
-function async createMockUser(data) {
-  return await 
-}
