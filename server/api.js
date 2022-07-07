b4592c8ad00a454417b79675ceea486f304b6bfe
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2022-07-07 16:58:38 +0500

    update api

--- server/api.js
+++ server/api.js
@@ -4,18 +4,8 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const db = require("./config/db.config.js");
+
 const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-const MONGODB_URI = `mongodb+srv://${db.MONGODB_USERNAME}:${db.MONGODB_PASSWORD}@${db.MONGODB_CLUSTER}/?retryWrites=true&w=majority`;
-mongoose.connect(MONGODB_URI);
-
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
-});
 
 const PORT = process.env.PORT;
 const HOST = process.env.HOST;
@@ -24,9 +14,20 @@ const notesRouter = require('./routes/notes');
 const usersRouter = require('./routes/users');
 const searchRouter = require('./routes/search');
 
+app.use(cors());
+app.use(express.json());
+
 app.use('/notes', notesRouter);
 app.use('/', usersRouter);
 app.use('/search', searchRouter);
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+const MONGODB_URI = `mongodb+srv://${db.MONGODB_USERNAME}:${db.MONGODB_PASSWORD}@${db.MONGODB_CLUSTER}/?retryWrites=true&w=majority`;
+mongoose.connect(MONGODB_URI);
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+  app.listen(PORT, HOST);
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
+
