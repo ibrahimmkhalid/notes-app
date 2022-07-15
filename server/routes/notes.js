e949ae9ce71c1db9ead1e8432545bb8c0d4c166b
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2022-07-15 22:48:09 +0500

    update notes routes

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const responseHelper = require('../service/response.js');
 const {verifyJWT, getAuthenticatedUser} = require("../middleware/authenticate.js");
 const {
   getAllNotes, 
@@ -8,62 +9,47 @@ const {
   editNoteByID
 } = require("../service/notes.js");
 
-router.route('/all').get(async (req, res) => {
-  const user = getAuthenticatedUser(req);
-  let notes;
-  try {
-    notes = await getAllNotes(user);
-    res.json(notes);
-  } catch (err) {
-    res.status(400).json('Error: ' + err);
-  }
+router.route('/all').get((req, res) => {
+  responseHelper(res, async () => {
+    const user = getAuthenticatedUser(req);
+    const notes = await getAllNotes(user);
+    return {notes};
+  });
 });
 
 router.route('/add/public').post((req, res) => {
   const data = req.body;
   data.owner = null;
-  
-  try {
-    addNewNote(data);
-    res.json('Note Added!');
-  } catch (err) {
-    res.status(400).json('Error: ' + err);
-  }
+  responseHelper(res, async () => {
+    let note = await addNewNote(data);
+    return {note};
+  });
 });
 
 router.route('/add/private').post(verifyJWT, (req, res) => {
   const data = req.body;
   data.owner = req.user.id;
-  
-  try {
-    addNewNote(data);
-    res.json('Private note Added!');
-  } catch (err) {
-    res.status(400).json('Error: ' + err);
-  }
+  responseHelper(res, async () => {
+    let note = await addNewNote(data);
+    return {note};
+  });
 });
 
-router.route('/:id').get(async (req, res) => {
+router.route('/:id').get((req, res) => {
   const data = req.params;
-  const user = getAuthenticatedUser(req);
-  let note;
-  try {
-    note = await getNoteByID(data, user);
-    res.json(note);
-  } catch (err) {
-    res.status(400).json('Error: ' + err);
-  }
+  responseHelper(res, async () => {
+    const user = getAuthenticatedUser(req);
+    const note = await getNoteByID(data, user);
+    return {note};
+  });
 });
 
 router.route('/:id').delete(async (req, res) => {
   const data = req.params;
-  const user = getAuthenticatedUser(req);
-  try {
+  responseHelper(res, async () => {
+    const user = getAuthenticatedUser(req);
     await deleteNoteByID(data, user);
-    res.json("Note Deleted!")
-  } catch (err) {
-    res.status(400).json('Error: ' + err);
-  }
+  });
 });
 
 router.route('/update/:id').post(async (req, res) => {
@@ -71,13 +57,10 @@ router.route('/update/:id').post(async (req, res) => {
     ...req.params,
     ...req.body
   };
-  const user = getAuthenticatedUser(req);
-  try {
+  responseHelper(res, async () => {
+    const user = getAuthenticatedUser(req);
     await editNoteByID(data, user);
-    res.json("Note Edited!");
-  } catch (err) {
-    res.status(400).json('Error: ' + err);
-  }
+  });
 });
 
 module.exports = router;
