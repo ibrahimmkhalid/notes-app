6ed197c6d8ad9d40193a6e60c49cf0de0e817692
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2022-07-15 21:52:11 +0500

    forgot to remove a testing line

--- server/routes/users.js
+++ server/routes/users.js
@@ -9,7 +9,7 @@ router.route('/register').post(async (req, res) => {
   const data = req.body;
   await responseHelper(res, async () => {
     const user = await createUser(data);
-    if (user) {
+    if (!user) {
       throw {general: ["There was an error creating your user"]};
     }
     const token = await loginUser(data);
