180d8180a93e41122ff0eb3ba8002a7a6e0ea0ac
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2022-07-18 18:21:21 +0500

    added test for notes empty

---
+++ server/test/notes.js
@@ -0,0 +1,33 @@
+process.env.NODE_ENV = 'test';
+
+let mongoose = require("mongoose")
+let User = require("../models/user.models.js");
+let Note = require("../models/note.models.js");
+
+let chai = require("chai");
+let request = require("supertest");
+let api = require("../api.js");
+
+const expect = chai.expect;
+
+//hash for 'password'
+const hashedPassword = '$2b$10$HsQ0cHHufbLisYbEQxoPkOCwzTomz.E7mLYVTEM34lj3VTNexTtsu';
+
+let mockUsersData = {
+  user1: {
+    username: 'user1',
+    password: hashedPassword,
+    admin: true,
+  },
+  user2: {
+    username: 'user2',
+    password: hashedPassword,
+    admin: false
+  },
+}
+
+describe('Notes', () => {
+  beforeEach((done) => {
+
+  });
+})
