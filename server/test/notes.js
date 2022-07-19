da37087fe7c2deb9fa6514d3aadcab3c4a07497e
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2022-07-19 13:32:12 +0500

    updated tests for notes

--- server/test/notes.js
+++ server/test/notes.js
@@ -13,28 +13,75 @@ const expect = chai.expect;
 //hash for 'password'
 const hashedPassword = '$2b$10$HsQ0cHHufbLisYbEQxoPkOCwzTomz.E7mLYVTEM34lj3VTNexTtsu';
 
-let mockUsersData = {
-  user1: {
+let mockUsersData = [
+  {
     username: 'user1',
     password: hashedPassword,
     admin: true,
   },
-  user2: {
+  {
     username: 'user2',
     password: hashedPassword,
     admin: false
   },
-}
+]
 
 describe('Notes', () => {
-  beforeAll((done) => {
-    User.create(mockUsersData, (err) => {
-      done();
+  before(async () => {
+    await User.create(mockUsersData);
+  });
+
+  describe('Getting all notes', () => {
+    before(async () => {
+      let user = await User.findOne({username: 'user1'});
+      await Note.create([
+        { title:'u1n1', text:'u1n1', owner: user.id },
+        { title:'u1n2', text:'u1n2', owner: user.id }
+      ]);
+      user = await User.findOne({username: 'user2'});
+      await Note.create([
+        { title:'u2n1', text:'u2n1', owner: user.id },
+        { title:'u2n2', text:'u2n2', owner: user.id }
+      ]);
+      await Note.create([
+        { title:'uxn1', text:'uxn1', owner: null },
+        { title:'uxn2', text:'uxn2', owner: null }
+      ]);
     });
+
+    it('gets all notes if user is an admin', async () => {
+      let token = await login(mockUsersData[0]);
+      request(api)
+        .get('/notes/all')
+        .set('Authorization', token)
+        .end((err, res) => {
+          expect(res.status).to.eq(200);
+          expect(res.body.data.notes).to.be.an('array');
+          expect(res.body.data.notes).to.have.lengthOf(6);
+        })
+    });
+
   });
-  beforeEach((done) => {
-    User.deleteMany({}, (err) => {
-      done();
-    })
+
+  after(async () => {
+    await User.deleteMany({});
+    await Note.deleteMany({});
   });
-})
+});
+
+
+async function login(user) {
+  if (user) {
+    let _data = {
+      username: user.username,
+      password: 'password'
+    };
+
+    let res = await request(api)
+      .post('/login')
+      .send(_data);
+    return 'Bearer ' + res.body.data.token;
+  } else {
+    return null;
+  }
+}
