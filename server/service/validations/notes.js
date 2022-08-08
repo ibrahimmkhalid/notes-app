0c22c67d5de68e91d2629c4a4f2f1cffa8958f36
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2022-08-08 21:49:21 +0500

    refactor

--- server/service/validations/notes.js
+++ server/service/validations/notes.js
@@ -1,64 +1,63 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 module.exports = {
-  addNewNote: function(data) {
-    let err = [];
+  addNewNote: function (data) {
+    let err = []
     if (!data.title) {
-      err.push("'title' is a required field");
+      err.push("'title' is a required field")
     }
 
     if (!data.text) {
-      err.push("'text' is a required field");
+      err.push("'text' is a required field")
     }
 
     if (err.length !== 0) {
       throw {
         code: 400,
-        validation: err
-      };
+        validation: err,
+      }
     }
   },
-  getNoteByID: function(data, user) {
-    let err = [];
+  getNoteByID: function (data, user) {
+    let err = []
     if (!data.id) {
-      err.push("'id' is a required field");
+      err.push("'id' is a required field")
     }
 
-    if(!mongoose.isValidObjectId(data.id)) {
-      err.push("invalid ObjectId")
+    if (!mongoose.isValidObjectId(data.id)) {
+      err.push('invalid ObjectId')
     }
 
     if (err.length !== 0) {
       throw {
         code: 400,
-        validation: err
-      };
+        validation: err,
+      }
     }
   },
-  deleteNoteByID: function(data, user) {
-    let err = [];
+  deleteNoteByID: function (data, user) {
+    let err = []
     if (!data.id) {
-      err.push("'id' is a required field");
+      err.push("'id' is a required field")
     }
 
     if (err.length !== 0) {
       throw {
         code: 400,
-        validation: err
-      };
+        validation: err,
+      }
     }
   },
-  editNoteByID: function(data, user) {
-    let err = [];
+  editNoteByID: function (data, user) {
+    let err = []
     if (!data.id) {
-      err.push("'id' is a required field");
+      err.push("'id' is a required field")
     }
 
     if (err.length !== 0) {
       throw {
         code: 400,
-        validation: err
-      };
+        validation: err,
+      }
     }
-  }
+  },
 }
-
