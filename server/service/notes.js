3091a0761352614cf0153957dbe262401687c4bf
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2022-08-12 20:14:16 +0500

    refactor

--- server/service/notes.js
+++ server/service/notes.js
@@ -1,71 +1,64 @@
-let Note = require('../models/note.models.js');
-const validation = require("./validations/notes.js");
+let Note = require('../models/note.models.js')
+const validation = require('./validations/notes.js')
 
 function checkIfUserCanAccessNote(user, note) {
-    
-  if(!
-      (!note.owner
-      || (note.owner && user && note.owner == user.id)
-      || (user && user.admin == true)
-      )
-    ) {
-      throw {
-        code: 401,
-        authentication: ["User does not have permission to access this note"]
-      };
+  if (
+    !(
+      !note.owner ||
+      (note.owner && user && note.owner == user.id) ||
+      (user && user.admin == true)
+    )
+  ) {
+    throw {
+      code: 401,
+      authentication: ['User does not have permission to access this note'],
     }
+  }
 }
 
 module.exports = {
-  getAllNotes: async function(user) {
+  getAllNotes: async function (user) {
     if (user && user.admin == true) {
-      return await Note.find();
+      return await Note.find()
     } else if (user && user.admin == false) {
-      return await Note.find().or([
-        {owner: null},
-        {owner: user.id}
-      ]);
+      return await Note.find().or([{ owner: null }, { owner: user.id }])
     } else {
-      return await Note.find().or([
-        {owner: null}
-      ]);
+      return await Note.find().or([{ owner: null }])
     }
   },
-  addNewNote: async function(data) {
-    validation.addNewNote(data);
+  addNewNote: async function (data) {
+    validation.addNewNote(data)
     const newNote = new Note({
       title: data.title,
       text: data.text,
       owner: data.owner,
-    });
-    return await newNote.save();
+    })
+    return await newNote.save()
   },
-  getNoteByID: async function(data, user) {
-    validation.getNoteByID(data);
-    const note = await Note.findById(data.id);
+  getNoteByID: async function (data, user) {
+    validation.getNoteByID(data)
+    const note = await Note.findById(data.id)
     if (!note) {
       throw {
         code: 404,
-        database: ["This resource could not be found"]
+        database: ['This resource could not be found'],
       }
     }
     checkIfUserCanAccessNote(user, note)
-    return note;
+    return note
   },
-  deleteNoteByID: async function(data, user) {
-    validation.deleteNoteByID(data);
-    const note = await Note.findById(data.id);
+  deleteNoteByID: async function (data, user) {
+    validation.deleteNoteByID(data)
+    const note = await Note.findById(data.id)
     checkIfUserCanAccessNote(user, note)
-    note.delete();
+    note.delete()
   },
-  editNoteByID: async function(data, user) {
-    validation.editNoteByID(data);
-    const note = await Note.findById(data.id);
+  editNoteByID: async function (data, user) {
+    validation.editNoteByID(data)
+    const note = await Note.findById(data.id)
     checkIfUserCanAccessNote(user, note)
     note.title = data.title ? data.title : note.title
     note.text = data.text ? data.text : note.text
-    note.save();
-
-  }
+    note.save()
+  },
 }
-
