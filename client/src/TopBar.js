b82a196381f799cf4991aecedfe96baf7cf4d5fb
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2022-08-21 19:26:18 +0500

    topbar update, remove bootstrap usage

--- client/src/TopBar.js
+++ client/src/TopBar.js
@@ -1,16 +1,11 @@
 function TopBar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-light top-bar">
-      <div className="container-fluid">
-        <span className="navbar-brand mb-0 h1">Notes App by @ibrahimmkhalid</span>
-        <form className="d-flex">
-          <input className="form-control me-2" type="text" placeholder="username" aria-label="username"></input>
-          <input className="form-control me-2" type="password" placeholder="password" aria-label="password"></input>
-          <button className="btn btn-outline-success" type="submit">Login/Register</button>
-        </form>
-      </div>
-    </nav>
+    <div className='top-bar'>
+      <span>
+        <h1>Notes App by @ibrahimmkhalid</h1>
+      </span>
+    </div>
   )
 }
 
-export default TopBar 
+export default TopBar
