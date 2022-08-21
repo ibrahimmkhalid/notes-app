2493902cdd7fb2e647346972ed695fa3952a6671
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2022-08-21 23:23:46 +0500

    pretty add note button

--- client/src/components/NotesList.js
+++ client/src/components/NotesList.js
@@ -1,18 +1,19 @@
-import { useEffect, useState } from 'react'
 import Note from './Note'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAdd } from '@fortawesome/free-solid-svg-icons'
+
 const NotesList = ({ notes }) => {
-  const [openNewNote, setOpenNewNote] = useState(false)
-  useEffect(() => {
-    console.log(notes)
-  }, [notes])
-
-
   return (
     <div className='notes-list'>
       {notes.map((note) => (
         <Note key={note.id} data={note} />
       ))}
 
+      <div className='note note-add'>
+        <div className='note-add-button'>
+          <FontAwesomeIcon icon={faAdd} className='note-add-button-icon' />
+        </div>
+      </div>
     </div>
   )
 }
