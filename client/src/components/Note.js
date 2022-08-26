a6aa0bdb1d83b5e50f44fa63ae1e455fa0486c75
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2022-08-27 01:16:46 +0500

    delete a note

--- client/src/components/Note.js
+++ client/src/components/Note.js
@@ -1,7 +1,21 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { endpointUrl } from '../helpers/urlHelpers'
 
 const Note = ({ data }) => {
+
+  const deleteNote = (event) => {
+    console.log(event)
+    const requestOptions = {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
+    }
+    let urlPart = `notes/${data.id}`
+    fetch(endpointUrl(urlPart), requestOptions)
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+  }
+
   return (
     <div className='note'>
       <div>
@@ -15,7 +29,7 @@ const Note = ({ data }) => {
       </div>
       <div className='note-footer'>
         <FontAwesomeIcon icon={faPenToSquare} />
-        <FontAwesomeIcon icon={faTrash} />
+        <FontAwesomeIcon onClick={deleteNote} icon={faTrash} />
       </div>
     </div>
   )
