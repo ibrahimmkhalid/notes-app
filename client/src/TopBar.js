a69838a5a24d314507b034729e1d6153207b85a5
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2023-02-03 22:00:14 -0800

    top bar changes

--- client/src/TopBar.js
+++ client/src/TopBar.js
@@ -1,9 +1,21 @@
+import { useState } from 'react'
+import Modal from './components/Modal'
+import UserPortal from './components/UserPortal'
+
 function TopBar() {
+  const [isUserModalOpen, setIsUserModalOpen] = useState(false)
+
   return (
     <div className='top-bar'>
       <span>
         <h1>Notes App by @ibrahimmkhalid</h1>
       </span>
+      <span>
+        <button onClick={() => setIsUserModalOpen(true)}>Login/Signup</button>
+      </span>
+      <Modal open={isUserModalOpen} onClose={() => setIsUserModalOpen(false)}>
+        <UserPortal></UserPortal>
+      </Modal>
     </div>
   )
 }
