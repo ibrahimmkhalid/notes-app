a376b9cabdccbda46ef99b0d0f2843a5f265d982
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2023-05-03 13:08:05 -0700

    update

---
+++ client/babel.config.js
@@ -0,0 +1,8 @@
+export const presets = [
+    "@babel/preset-env",
+    "@babel/preset-react"
+];
+export const plugins = [
+    ["@babel/plugin-proposal-decorators", { "decoratorsBeforeExport": true }],
+    "@babel/plugin-proposal-class-properties"
+];
--- client/src/stores/userStore.js
+++ client/src/stores/userStore.js
@@ -1,24 +1,55 @@
-import { action, computed, observable } from "mobx";
+// import { action, computed, observable } from "mobx";
+// import { createContext } from "react";
+//
+// class UserStore {
+//   @observable username = ""
+//   @observable isAdmin = false
+//   @observable token = ""
+//
+//   @action loadUser = (data) => {
+//     this.username = data.username
+//     this.isAdmin = data.admin
+//     this.token = data.token
+//   }
+//
+//   @action logout = () => {
+//     this.username = ""
+//     this.isAdmin = false
+//     this.token = ""
+//   }
+//
+//   @computed isLoggedIn = () => {
+//     return this.token !== ""
+//   }
+//
+// }
+//
+// export default createContext(new UserStore())
+
+import { makeAutoObservable } from "mobx";
 import { createContext } from "react";
 
 class UserStore {
-  @observable username = ""
-  @observable isAdmin = false
-  @observable token = ""
+  username = ""
+  isAdmin = false
+  token = ""
 
-  @action loadUser = (data) => {
+  constructor() {
+    makeAutoObservable(this)
+  }
+  loadUser = (data) => {
     this.username = data.username
     this.isAdmin = data.admin
     this.token = data.token
   }
 
-  @action logout = () => {
+  logout = () => {
     this.username = ""
     this.isAdmin = false
     this.token = ""
   }
 
-  @computed isLoggedIn = () => {
+  isLoggedIn = () => {
     return this.token !== ""
   }
 
