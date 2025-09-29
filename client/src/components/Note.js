2124d44d48f564e8218b87e4cc766cb21aa6b148
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2025-09-29 16:05:50 -0700

    wip

--- client/src/components/Note.js
+++ client/src/components/Note.js
@@ -1,50 +1,50 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
-import { endpointUrl } from '../helpers/urlHelpers'
-import { observer } from 'mobx-react'
-import { useContext } from 'react'
-import UserStore from '../stores/userStore'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { endpointUrl } from "../helpers/urlHelpers";
+import { observer } from "mobx-react";
+import { useContext } from "react";
+import UserStore from "../stores/userStore";
 
 const Note = ({ data, props }) => {
-  const userStore = useContext(UserStore)
+  const userStore = useContext(UserStore);
   const deleteNote = (event) => {
-    let requestOptions = {}
+    let requestOptions = {};
     requestOptions = {
-      method: 'DELETE',
-      headers: { 'Content-Type': 'application/json' },
-    }
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    };
     if (userStore.isLoggedIn) {
       requestOptions = {
-        method: 'DELETE',
+        method: "DELETE",
         headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${userStore.token}`
-        }
-      }
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userStore.token}`,
+        },
+      };
     }
-    let urlPart = `notes/${data.id}`
+    let urlPart = `notes/${data.id}`;
     fetch(endpointUrl(urlPart), requestOptions)
       .then((response) => response.json())
-      .then((data) => console.log(data))
-    props.reloadNotesList()
-  }
+      .then((data) => console.log(data));
+    props.reloadNotesList();
+  };
 
   return (
-    <div className='note'>
+    <div className="note">
       <div>
-        <div className='note-header'>
-          <span className='note-title'>{data.title}</span>
-          <span className='note-owner'>
-            {!data.owner ? 'public' : data.owner}
+        <div className="note-header">
+          <span className="note-title">{data.title}</span>
+          <span className="note-owner">
+            {!data.owner ? "public" : data.owner}
           </span>
         </div>
         <span>{data.text}</span>
       </div>
-      <div className='note-footer'>
+      <div className="note-footer">
         <FontAwesomeIcon onClick={deleteNote} icon={faTrash} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default observer(Note)
+export default observer(Note);
